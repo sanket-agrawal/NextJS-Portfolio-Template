17d54ae5392f7f581b4d942fa86bce623154b0f0
Sanket <mailsanketagrawal@gmail.com>
2025-09-27 21:35:08 +0530

    added vercel analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import "./globals.css";
 import Navbar from "./components/Navbar";
 import { seo } from "@/data/data";
 import Footer from "./components/Footer";
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata = {
     title: `${seo.title}`,
